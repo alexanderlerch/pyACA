e5be829ecdc581d0a5e6f5c6fabbac12c66497fd
alexander lerch <alexander.lerch@gatech.edu>
2019-07-08 10:43:42 +0800

    - fixes for time features - comments etc.

--- computeFeature.py
+++ computeFeature.py
@@ -30,23 +30,25 @@ def computeFeature(cFeatureName, afAudioData, f_s, afWindow=None, iBlockLength=4
     mypackage = __import__('Feature' + cFeatureName)
     hFeatureFunc = getattr(mypackage, 'Feature' + cFeatureName)
 
+    # pre-processing: downmixing 
+    if afAudioData.ndim > 1:
+        afAudioData = afAudioData.mean(axis=1)
+    
+    # pre-processing: normalization
+    fNorm = np.max(np.abs(afAudioData));
+    if fNorm != 0:
+        afAudioData = afAudioData/fNorm
+   
+    # pad with block length zeros just to make sure it runs for weird inputs, too
+    afAudioData = np.concatenate((afAudioData,np.zeros([iBlockLength,])),axis = 0)         
+
     if isSpectral(cFeatureName):
         # compute window function for FFT
         if afWindow is None:
             afWindow = ToolComputeHann(iBlockLength)
     
         assert(afWindow.shape[0] == iBlockLength), "parameter error: invalid window dimension"
-    
-        # pre-processing: downmixing 
-        if afAudioData.ndim > 1:
-            afAudioData = afAudioData.mean(axis=1)
         
-        # pre-processing: normalization
-        fNorm = np.max(np.abs(afAudioData));
-        if fNorm != 0:
-            afAudioData = afAudioData/fNorm
-   
-        afAudioData = np.concatenate((afAudioData,np.zeros([iBlockLength,])),axis = 0)         
         # in the real world, we would do this block by block...
         [f,t,X] = spectrogram(  afAudioData,
                                 f_s,
@@ -58,7 +60,7 @@ def computeFeature(cFeatureName, afAudioData, f_s, afWindow=None, iBlockLength=4
                                 True,
                                 'spectrum')
     
-        #  scale the same as for matlab
+        # we just want the magnitude spectrum...
         X = np.sqrt(X/2)
     
         # compute instantaneous pitch chroma
@@ -66,9 +68,12 @@ def computeFeature(cFeatureName, afAudioData, f_s, afWindow=None, iBlockLength=4
     
     if isTemporal(cFeatureName):
         [v,t] = hFeatureFunc(afAudioData, iBlockLength, iHopLength, f_s)
+        #[v,t] = hFeatureFunc(afAudioData, iBlockLength, iHopLength, f_s, np.array([2, 3]))
         
     return (v,t)
-    
+
+#######################################################   
+# helper functions
 def isSpectral(cName):
     bResult = False
     if "Spectral" in cName: 
@@ -82,15 +87,22 @@ def isTemporal(cName):
         bResult = True
         
     return (bResult)
+ 
     
-def computeFeatureCl(cPath, cFeatureName, bPlotOutput):
+
+
+
+def computeFeatureCl(cPath, cFeatureName, bPlotOutput = False):
     from ToolReadAudio import ToolReadAudio
-    
+  
+    # read audio file
     [f_s,afAudioData] = ToolReadAudio(cPath)
     #afAudioData = np.sin(2*np.pi * np.arange(f_s*1)*440./f_s)
  
+    # compute feature
     [v,t] = computeFeature(cFeatureName, afAudioData, f_s)
 
+    # plot feature output
     if bPlotOutput:
         plt.plot(t,v)
         
@@ -98,7 +110,8 @@ def computeFeatureCl(cPath, cFeatureName, bPlotOutput):
     
 if __name__ == "__main__":
     import argparse
-    
+
+    # add command line args and parse them    
     parser = argparse.ArgumentParser(description='Compute key of wav file')
     parser.add_argument('--infile', metavar='path', required=False,
                         help='path to input audio file')
@@ -106,19 +119,20 @@ if __name__ == "__main__":
                         help='feature name in the format SpectralPitchChroma')
     parser.add_argument('--plotoutput', metavar='bool', required=False,
                         help='option to plot the output')
-    
+ 
+    # retrieve command line args
     cPath = parser.parse_args().infile
     cFeatureName = parser.parse_args().featurename
     bPlotOutput  = parser.parse_args().plotoutput
     
     #only for debugging
-    if not cPath:
-        cPath = "c:/temp/test.wav"
-    #only for debugging
-    if not cFeatureName:
-        cFeatureName = "SpectralDecrease"
-    if not bPlotOutput:
-        bPlotOutput = True
+    if __debug__:
+        if not cPath:
+            cPath = "c:/temp/test.wav"
+        if not cFeatureName:
+            cFeatureName = "TimeAcfCoeff"
+        if not bPlotOutput:
+            bPlotOutput = True
     
     # call the function
     computeFeatureCl(cPath, cFeatureName, bPlotOutput)
