d1a2dbeec94d41780e9d9eecda08c3b8b5f0ab63
alexander lerch <alexander.lerch@gatech.edu>
2019-07-12 15:25:37 +0800

    added Spectral Slope

---
+++ FeatureSpectralSlope.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+computes the spectral slope from the magnitude spectrum
+
+  Args:
+    X: spectrogram (dimension FFTLength X Observations)
+    f_s: sample rate of audio data
+
+  Returns:
+    vssl spectral slope
+"""
+
+import numpy as np
+  
+    
+def FeatureSpectralSlope(X,f_s):   
+    
+    # compute mean
+    mu_x = X.mean(axis=0,keepdims=True)
+
+    # compute index vector
+    kmu = np.arange(0, X.shape[0]) - X.shape[0]/2
+ 
+    # compute slope
+    X = X - mu_x
+    vssl = np.dot(kmu, X)/np.dot(kmu,kmu)
+
+    return (vssl)
