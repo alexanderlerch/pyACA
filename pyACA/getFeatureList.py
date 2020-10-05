30662108d9548a4f29746b07df02999979be4b7b
Kaushal Sali <salikaushal@gmail.com>
2020-10-05 16:47:05 -0400

    getFeatureList argument made case insensitive. Invalid arg warning added.

--- pyACA/getFeatureList.py
+++ pyACA/getFeatureList.py
@@ -2,7 +2,7 @@
 Forms a list of available features by checking for all the Feature*.py files present in the package.
 
   Args:
-    type: type of Features (valid values: 'all', 'spectral' 'temporal')
+    feature_type: (optional) type of features (valid values: 'all', 'spectral' 'temporal')
 
   Returns:
     features:  list of strings
@@ -17,12 +17,16 @@ import pyACA
 
 def getFeatureList(feature_type='all'):
 
+    feature_type = feature_type.lower()
     if feature_type == 'all':
         feature_type = ''
     elif feature_type == 'spectral':
         feature_type = 'Spectral'
     elif feature_type == 'temporal':
         feature_type = 'Time'
+    else:
+        print('Invalid feature type')
+        return []
 
     package_loc = os.path.dirname(pyACA.__file__)
     modules = sorted(glob.glob(os.path.join(package_loc, 'Feature' + feature_type + '*.py')))
