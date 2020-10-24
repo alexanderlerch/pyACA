bf7826554cf482a8cd74201b2d46480b9db24592
Kaushal Sali <salikaushal@gmail.com>
2020-10-24 16:21:39 -0400

    Tests for checking output shape of spectral features for different types of input (without using computeFeature) added.

--- tests/test_shape.py
+++ tests/test_shape.py
@@ -22,5 +22,74 @@ class TestShape(unittest.TestCase):
                 npt.assert_equal(out.shape[-1], expectedNumBlocks)
 
 
+    def test_spectral_shape_spectrum_input(self):
+        fs = 44100
+        X = np.zeros(1025)
+
+        features = pyACA.getFeatureList('spectral')
+        features.remove('SpectralMfccs')
+        features.remove('SpectralPitchChroma')
+
+        for feature in features:
+            with self.subTest(msg=feature):
+                featureFunc = getattr(pyACA, "Feature" + feature)
+                out = featureFunc(X, fs)
+                npt.assert_equal(out.shape, ())
+
+        with self.subTest(msg='SpectralMfccs'):
+            out = pyACA.FeatureSpectralMfccs(X, fs)
+            npt.assert_equal(out.shape, (13,))
+
+        with self.subTest(msg='SpectralPitchChroma'):
+            out = pyACA.FeatureSpectralPitchChroma(X, fs)
+            npt.assert_equal(out.shape, (12,))
+
+
+    def test_spectral_shape_spectrogram_single_block_input(self):
+        X = np.zeros((1025, 1))
+        fs = 44100
+
+        features = pyACA.getFeatureList('spectral')
+        features.remove('SpectralMfccs')
+        features.remove('SpectralPitchChroma')
+
+        for feature in features:
+            with self.subTest(msg=feature):
+                featureFunc = getattr(pyACA, "Feature" + feature)
+                out = featureFunc(X, fs)
+                npt.assert_equal(out.shape, (1,))
+
+        with self.subTest(msg='SpectralMfccs'):
+            out = pyACA.FeatureSpectralMfccs(X, fs)
+            npt.assert_equal(out.shape, (13, 1))
+
+        with self.subTest(msg='SpectralPitchChroma'):
+            out = pyACA.FeatureSpectralPitchChroma(X, fs)
+            npt.assert_equal(out.shape, (12, 1))
+
+
+    def test_spectral_shape_spectrogram_multiple_blocks_input(self):
+        X = np.zeros((1025, 16))
+        fs = 44100
+
+        features = pyACA.getFeatureList('spectral')
+        features.remove('SpectralMfccs')
+        features.remove('SpectralPitchChroma')
+
+        for feature in features:
+            with self.subTest(msg=feature):
+                featureFunc = getattr(pyACA, "Feature" + feature)
+                out = featureFunc(X, fs)
+                npt.assert_equal(out.shape, (16,))
+
+        with self.subTest(msg='SpectralMfccs'):
+            out = pyACA.FeatureSpectralMfccs(X, fs)
+            npt.assert_equal(out.shape, (13, 16))
+
+        with self.subTest(msg='SpectralPitchChroma'):
+            out = pyACA.FeatureSpectralPitchChroma(X, fs)
+            npt.assert_equal(out.shape, (12, 16))
+
+
     def calcNumBlocks(self, inputSize, blockLen, hopLen):
         return np.floor((inputSize - blockLen) / hopLen + 1)
