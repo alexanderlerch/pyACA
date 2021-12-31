fixed spectrogram normalization with additional normalization flag

pyACA/ToolPreprocAudio.py
@@ -5,6 +5,7 @@ helper function: read audio from wav
   Args:
     afAudioData: audio file data
     iBlockLength: processing block length
+    bNormalize: flag to switch off normalization (optional)
 
   Returns:
     afAudioData (array): processed samples
@@ -13,16 +14,17 @@ helper function: read audio from wav
 import numpy as np
 
 
-def ToolPreprocAudio(afAudioData, iBlockLength):
+def ToolPreprocAudio(afAudioData, iBlockLength, bNormalize=True):
 
     # pre-processing: downmixing
     if afAudioData.ndim > 1:
         afAudioData = afAudioData.mean(axis=1)
     
     # pre-processing: normalization
-    fNorm = np.max(np.abs(afAudioData))
-    if fNorm != 0:
-        afAudioData = afAudioData / fNorm
+    if bNormalize:
+        fNorm = np.max(np.abs(afAudioData))
+        if fNorm != 0:
+            afAudioData = afAudioData / fNorm
 
     # additional preprocessing step might include sample rate conversion and filtering
     

pyACA/computeSpectrogram.py
@@ -24,10 +24,13 @@ from pyACA.ToolComputeHann import ToolComputeHann
 from pyACA.ToolBlockAudio import ToolBlockAudio
 
 
-def computeSpectrogram(afAudioData, f_s, afWindow=None, iBlockLength=4096, iHopLength=2048):
+def computeSpectrogram(afAudioData, f_s, afWindow=None, iBlockLength=4096, iHopLength=2048, bNormalize=True):
 
-    # Pre-process: down-mix, normalize, zero-pad
-    afAudioData = ToolPreprocAudio(afAudioData, iBlockLength)
+    iBlockLength = np.int_(iBlockLength)
+    iHopLength = np.int_(iHopLength)
+
+    # Pre-process: down-mix, normalize
+    afAudioData = ToolPreprocAudio(afAudioData, iBlockLength, bNormalize)
 
     if afWindow is None:
         # Compute window function for FFT
@@ -44,7 +47,7 @@ def computeSpectrogram(afAudioData, f_s, afWindow=None, iBlockLength=4096, iHopL
 
     for n in range(0, xb.shape[0]):
         # apply window
-        tmp = abs(np.fft.fft(xb[n, :] * afWindow)) / xb.shape[1]
+        tmp = abs(np.fft.fft(xb[n, :] * afWindow)) * 2 / xb.shape[1]
 
         # compute magnitude spectrum
         X[:, n] = tmp[range(iSpecDim[0])]

tests/test_tools.py
@@ -71,7 +71,7 @@ class TestTools(unittest.TestCase):
         t = np.arange(0, fs) / fs
         x = A*np.sin(2 * np.pi * f0 * t)
 
-        [X, f, t] = pyACA.computeSpectrogram(x, fs, np.ones(iBlockLength), iBlockLength, iHopLength)
+        [X, f, t] = pyACA.computeSpectrogram(x, fs, np.ones(iBlockLength), iBlockLength, iHopLength, bNormalize=False)
 
         npt.assert_almost_equal(X[np.int_(f0)][0], A, decimal=7, err_msg="SP 6: magnitude spectrum incorrect")
         npt.assert_almost_equal(np.sum(X), A, decimal=7, err_msg="SP 7: magnitude spectrum incorrect")