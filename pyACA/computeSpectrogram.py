fixes spectrogram scaling

pyACA/computeSpectrogram.py
@@ -44,11 +44,12 @@ def computeSpectrogram(afAudioData, f_s, afWindow=None, iBlockLength=4096, iHopL
 
     for n in range(0, xb.shape[0]):
         # apply window
-        tmp = abs(np.fft.fft(xb[n, :] * afWindow)) * 2 / xb.shape[1]
+        tmp = abs(np.fft.fft(xb[n, :] * afWindow)) / xb.shape[1]
 
         # compute magnitude spectrum
         X[:, n] = tmp[range(iSpecDim[0])]
-        X[[0, iSpecDim[0]-1], n] = X[[0, iSpecDim[0]-1], n] / np.sqrt(2)  # let's be pedantic about normalization
+
+    X[[0, iSpecDim[0]-1], :] = X[[0, iSpecDim[0]-1], :] / np.sqrt(2)  # let's be pedantic about normalization
 
     f = np.arange(0, iSpecDim[0]) * f_s / iBlockLength
 

tests/test_tools.py
@@ -49,7 +49,7 @@ class TestTools(unittest.TestCase):
         t = np.arange(0, fs - 1) / fs
         x = np.sin(2 * np.pi * f * t)
 
-        [X, f, t] = pyACA.computeSpectrogram(x, fs, None, iBlockLength, iHopLength,)
+        [X, f, t] = pyACA.computeSpectrogram(x, fs, None, iBlockLength, iHopLength)
 
         # spectrum length
         self.assertEqual(X.shape[0], (iBlockLength // 2 + 1), "SP 1: number of frequency bins incorrect")
@@ -61,6 +61,20 @@ class TestTools(unittest.TestCase):
         # content
         npt.assert_almost_equal(X[10][10] / X[20][10], 4154.15, decimal=2, err_msg="SP 4: magnitude spectrum incorrect")
         npt.assert_almost_equal(X[115][10] / X[116][10], 1.029, decimal=2, err_msg="SP 5: magnitude spectrum incorrect")
+        
+        # non-windowed content (freq on bin)
+        f0 = 4
+        fs = 16
+        A = 0.5
+        iBlockLength = 16
+        iHopLength = 16
+        t = np.arange(0, fs) / fs
+        x = A*np.sin(2 * np.pi * f0 * t)
+
+        [X, f, t] = pyACA.computeSpectrogram(x, fs, np.ones(iBlockLength), iBlockLength, iHopLength)
+
+        npt.assert_almost_equal(X[np.int_(f0)][0], A, decimal=7, err_msg="SP 6: magnitude spectrum incorrect")
+        npt.assert_almost_equal(np.sum(X), A, decimal=7, err_msg="SP 7: magnitude spectrum incorrect")
 
 
     def test_freq2midi2freq(self):