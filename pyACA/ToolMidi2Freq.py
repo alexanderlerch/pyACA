6fd110734afd8e74fab30c1024adf313a7021a02
alexanderlerch <alexander.lerch@gatech.edu>
2021-12-30 22:38:37 -0500

    fixed copy and paste errors in ToolMidi2Freq.py

--- pyACA/ToolMidi2Freq.py
+++ pyACA/ToolMidi2Freq.py
@@ -16,7 +16,7 @@ import numpy as np
 def ToolMidi2Freq(pInMidi, fA4InHz = 440):
     def convert_midi2freq_scalar(p, fA4InHz):
  
-        if f <= 0:
+        if p < 0:
             return 0
         else:
             return fA4InHz * 2**((p-69) / 12)
@@ -29,4 +29,4 @@ def ToolMidi2Freq(pInMidi, fA4InHz = 440):
     for k,p in enumerate(pInMidi):
         fInHz[k] =  convert_midi2freq_scalar(p,fA4InHz)
             
-    return (midi)
+    return (fInHz)
