5ab40c08c6b436190d64f5aa83969908ac2c095e
alexanderlerch <alexander.lerch@gatech.edu>
2021-12-31 21:58:19 -0500

    added a few feature tests

--- tests/test_features.py
+++ tests/test_features.py
@@ -110,5 +110,61 @@ class TestFeatures(unittest.TestCase):
         vnf = pyACA.NoveltyFlux(X, -1)
         npt.assert_almost_equal(vnf[1], np.sqrt(5) / 5, decimal=7, err_msg="NF 2: hwr incorrect")
 
+    def test_rms(self):
+
+        # zero test
+        iBlockLength = 4096
+        iHopLength = 2048
+        x = np.zeros(iBlockLength)
+        fs = 4096
+
+        vrms, t = pyACA.FeatureTimeRms(x, iBlockLength, iHopLength, fs)
+        self.assertEqual(vrms[0], -100, "RMS 1: feature value incorrect")
+
+        # sine test
+        f0 = 4
+        fs = 256
+        A = 1
+        iBlockLength = 128
+        iHopLength = 64
+        t = np.arange(0, fs) / fs
+        x = A*np.sin(2 * np.pi * f0 * t)
+
+        vrms, t = pyACA.FeatureTimeRms(x, iBlockLength, iHopLength, fs)
+        self.assertEqual(t[1]-t[0], .25, "RMS 2: time stamps incorrect")
+        npt.assert_almost_equal(vrms[1], 20*np.log10(np.sqrt(2)/2), decimal=7, err_msg="RMS 3: feature value incorrect")
+
+    def test_zerocrossingrate(self):
+        fs = 44100
+        f = 440.
+        iBlockLength = 882
+        iHopLength = 441
+    
+        x = np.sin(2*np.pi * np.arange(fs*1)*f/fs)
+    
+        # sine input
+        vzcr, t = pyACA.FeatureTimeZeroCrossingRate(x, iBlockLength, iHopLength, fs)
+
+        npt.assert_almost_equal(vzcr[1], f*2/fs, decimal=2, err_msg="ZCR 1: feature value incorrect")
+
+    def test_spectral_flux(self):
+        X = np.zeros(1025)
+        fs = 1
+
+        # zero input
+        vsf = pyACA.FeatureSpectralFlux(X, fs)
+        self.assertEqual(vsf, 0, "SF 1: Zero input incorrect")
+
+        # one input
+        X = np.ones([5, 2])
+        vsf = pyACA.FeatureSpectralFlux(X, -1)
+        npt.assert_almost_equal(vsf[1], 0, decimal=7, err_msg="SF 2: value incorrect")
+
+        X = np.ones([5, 2])
+        X[:, 0] = 0
+        vsf = pyACA.FeatureSpectralFlux(X, -1)
+        npt.assert_almost_equal(vsf[1], np.sqrt(5) / 5, decimal=7, err_msg="SF 3: value incorrect")
+
+
 if __name__ == '__main__':
     unittest.main()
