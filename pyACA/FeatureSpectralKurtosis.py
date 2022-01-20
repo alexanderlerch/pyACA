63788f5b143870b071cb47770abc2b150abacbde
alexanderlerch <alexander.lerch@gatech.edu>
2022-01-20 17:50:12 -0500

    removed legacy implementation compute in bin domain instead of frequency domain

--- pyACA/FeatureSpectralKurtosis.py
+++ pyACA/FeatureSpectralKurtosis.py
@@ -15,35 +15,24 @@ from .FeatureSpectralCentroid import FeatureSpectralCentroid
 from .FeatureSpectralSpread import FeatureSpectralSpread
 
 
-def FeatureSpectralKurtosis(X, f_s, UseBookDefinition=False):
+def FeatureSpectralKurtosis(X, f_s):
 
     isSpectrum = X.ndim == 1
     if isSpectrum:
         X = np.expand_dims(X, axis=1)
 
-    if UseBookDefinition:  # not recommended
-        # compute mean and standard deviation
-        mu_x = np.mean(X, axis=0, keepdims=True)
-        std_x = np.std(X, axis=0)
+    k = np.arange(0, X.shape[0]) 
+    # get spectral centroid and spread (mean and std of dist)
+    vsc = FeatureSpectralCentroid(X, f_s)  * 2 / f_s * (X.shape[0]-1)
+    vss = FeatureSpectralSpread(X, f_s)    * 2 / f_s * (X.shape[0]-1)
 
-        # remove mean
-        X = X - mu_x
+    norm = X.sum(axis=0)
+    norm[norm == 0] = 1
+    vss[vss == 0] = 1
 
-        # compute kurtosis
-        vsk = np.sum(X**4, axis=0) / (std_x**4 * X.shape[0])
-    else:
-        f = np.arange(0, X.shape[0]) / (X.shape[0] - 1) * f_s / 2
-        # get spectral centroid and spread (mean and std of dist)
-        vsc = FeatureSpectralCentroid(X, f_s)  # *2/f_s * (X.shape[0]-1)
-        vss = FeatureSpectralSpread(X, f_s)    # *2/f_s * (X.shape[0]-1)
-
-        norm = X.sum(axis=0)
-        norm[norm == 0] = 1
-        vss[vss == 0] = 1
-
-        # compute kurtosis
-        vsk = np.zeros(X.shape[1])
-        for n in range(0, X.shape[1]):
-            vsk[n] = np.dot((f - vsc[n])**4, X[:, n]) / (vss[n]**4 * norm[n] * X.shape[0])
+    # compute kurtosis
+    vsk = np.zeros(X.shape[1])
+    for n in range(0, X.shape[1]):
+        vsk[n] = np.dot((k - vsc[n])**4, X[:, n]) / (vss[n]**4 * norm[n] * X.shape[0])
 
     return np.squeeze(vsk - 3) if isSpectrum else (vsk - 3)
